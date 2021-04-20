29bc949c05d19ec08aae5901e12f1882f1467467
Alejandro Pedraza <alejandro@buoyant.io>
2021-04-20 17:46:10 -0500

    Improve error handling

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use env_logger::Builder;
-use handlebars::{to_json, Handlebars};
+use handlebars::{to_json, Handlebars, RenderError};
 use hyper::{body, Client as HyperClient};
 use k8s_openapi::api::core::v1::{Pod, Service};
 use kube::{
@@ -12,6 +12,8 @@ use serde::{Deserialize, Serialize};
 use serde_json::value::Map;
 use std::{env, time};
 use structopt::StructOpt;
+
+type Error = Box<dyn std::error::Error + Send + Sync>;
 
 #[derive(Clone, Debug, Serialize, StructOpt)]
 #[structopt(about = "Service failover operator for Kubernetes")]
@@ -50,22 +52,29 @@ pub struct Backend {
 }
 
 #[tokio::main]
-async fn main() -> Result<(), kube::Error> {
+async fn main() -> Result<(), Error> {
     let mut builder = Builder::new();
     builder.parse_default_env();
     builder.init();
 
     let opt = Opt::from_args();
     if env::var("OPERATOR").is_err() {
-        render_manifests(opt);
+        render_manifests(opt)?;
         return Ok(());
     }
 
     let client = Client::try_default().await?;
     let api_svc: Api<Service> = Api::namespaced(client, &opt.namespace);
     let svc = api_svc.get(&opt.svc_watch).await?;
-    let svc_selector = svc.spec.unwrap().selector.unwrap();
-    let (key, val) = svc_selector.iter().next().unwrap();
+    let svc_selector = svc
+        .spec
+        .expect("Couldn't retrieve spec from service")
+        .selector
+        .expect("Couldn't retrieve selector from service spec");
+    let (key, val) = svc_selector
+        .iter()
+        .next()
+        .expect("Service selector is empty");
     let client = Client::try_default().await?;
     let api_pod: Api<Pod> = Api::namespaced(client, &opt.namespace);
     let pods = api_pod
@@ -73,7 +82,21 @@ async fn main() -> Result<(), kube::Error> {
         .await?;
     let ips = pods
         .iter()
-        .map(|pod| pod.status.as_ref().unwrap().pod_ip.as_ref().unwrap())
+        .inspect(|pod| {
+            if pod.status.is_none() {
+                log::warn!(
+                    "Pod '{}'doesn't have a status",
+                    pod.metadata.name.as_ref().unwrap_or(&"unknown".to_string())
+                );
+            }
+        })
+        .filter_map(|pod| pod.status.as_ref())
+        .inspect(|status| {
+            if status.pod_ip.is_none() {
+                log::warn!("Pod doesn't have an IP");
+            }
+        })
+        .filter_map(|status| status.pod_ip.as_ref())
         .collect();
     poll_metrics(ips, opt.min_success_rate).await?;
     log::info!(
@@ -87,32 +110,33 @@ async fn main() -> Result<(), kube::Error> {
     Ok(())
 }
 
-fn render_manifests(opt: Opt) {
+fn render_manifests(opt: Opt) -> Result<(), RenderError> {
     let mut data = Map::new();
     data.insert("args".to_string(), to_json(&opt));
 
     let mut hb = Handlebars::new();
     hb.set_strict_mode(true);
     hb.register_template_string("manifests", include_str!("templates/manifests.yml"))
-        .unwrap();
-    println!("{}", hb.render("manifests", &to_json(&data)).unwrap());
+        .expect("manifests.yml not found");
+    hb.render("manifests", &to_json(&data))
+        .map(|x| println!("{}", x))
 }
 
-async fn poll_metrics(ips: Vec<&String>, min_success_rate: f64) -> Result<(), kube::Error> {
+async fn poll_metrics(ips: Vec<&String>, min_success_rate: f64) -> Result<(), Error> {
     let client = HyperClient::new();
     let re_success: Regex = Regex::new(
         r#"(?m)^response_total\{.*target_addr=".*:(\d+)".*classification="success".* (\d+)"#,
     )
-    .unwrap();
+    .expect("Failed parsing re_success");
     let re_failure: Regex = Regex::new(
         r#"(?m)^response_total\{.*target_addr=".*:(\d+)".*classification="failure".* (\d+)"#,
     )
-    .unwrap();
+    .expect("Failed parsing re_failure");
     loop {
         let mut total_success = 0.0;
         let mut total_failure = 0.0;
         for ip in &ips {
-            let uri = format!("http://{}:4191/metrics", ip).parse().unwrap();
+            let uri = format!("http://{}:4191/metrics", ip).parse()?;
             let resp = client.get(uri).await?;
             let bytes = body::to_bytes(resp.into_body()).await?;
             let body = String::from_utf8(bytes.to_vec())?;
@@ -120,13 +144,19 @@ async fn poll_metrics(ips: Vec<&String>, min_success_rate: f64) -> Result<(), ku
                 if &cap[1] == "4191" {
                     continue;
                 }
-                total_success = total_success + cap[2].parse::<f64>().unwrap();
+                match cap[2].parse::<f64>() {
+                    Ok(x) => total_success = total_success + x,
+                    Err(x) => log::warn!("Invalid metric value: {}: {}", cap[2].to_string(), x),
+                }
             }
             for cap in re_failure.captures_iter(&body) {
                 if &cap[1] == "4191" {
                     continue;
                 }
-                total_failure = total_failure + cap[2].parse::<f64>().unwrap();
+                match cap[2].parse::<f64>() {
+                    Ok(x) => total_failure = total_failure + x,
+                    Err(x) => log::warn!("Invalid metric value: {}: {}", cap[2].to_string(), x),
+                }
             }
         }
         let success_rate = total_success / (total_success + total_failure);
